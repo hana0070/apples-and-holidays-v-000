d4fd762843548eeb07fd1d6d7418128b38a09be1
HANA LEANDER <hana0070@gmail.com>
2019-04-22 22:51:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
-    end
-    end
+      end
+    end.flatten
   end
 end
