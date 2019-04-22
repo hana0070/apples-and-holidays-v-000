e5753d37540887f954d396fb2bb04fdfef6928cf
HANA LEANDER <hana0070@gmail.com>
2019-04-22 22:33:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |holiday, supplies|
-    holiday_hash.flatten
+    holiday.each do |supplies|
+      
   end
 end
 
