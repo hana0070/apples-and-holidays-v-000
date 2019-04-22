7e2ed3fad251bfa8f27e503b43f4a6b909982f21
HANA LEANDER <hana0070@gmail.com>
2019-04-22 22:10:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     season.each do |holiday|
-      if season == :winter
+      if season == [:winter]
         holiday.each do |activity|
           activity << "Balloons"
         end
